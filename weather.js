07b4f54a4982e0692bd0760c5db0400d50dedd91
Shane <shane.cmetzger@gmail.com>
2015-02-23 17:05:02 -0500

    Weather
    Here’s the weather so far. We’ll use radio buttons instead and have
    users click the button to display info. I can include other weather
    related reports that the API has. But later tonight I’ll style that and
    the clock so they look similar. Most of it just needs some cleaning up
    but otherwise let me know.

---
+++ weather.js
@@ -0,0 +1,39 @@
+
+(function ($) {
+
+  $(document).ready(function() {
+    
+    $("input[type='button']").click(function(){
+        var radioValue = $("input[name='city']:checked").val();
+    $('#weather_city').val(radioValue);
+    processForm();
+    
+    });
+  });
+ 
+  /**
+   * Callback to process the form.
+   */
+  function processForm() {
+    // Fetch the data from the public API through JSONP.
+    // See http://openweathermap.org/API#weather.
+
+    $.ajax({
+      url: 'http://api.openweathermap.org/data/2.5/weather',
+      cache: false,
+      data: {
+        q: $('#weather_city').val()
+      },
+      // work with the response
+      success: function (response) {
+        $('#weather_description').text(response.weather[0].description);
+        $('#weather_country').text(response.sys.country);
+        $('#weather_temp').text(Math.round((9/5)*(response.main.temp - 273) + 32));
+        $('#weather_humidity').text(response.main.humidity);
+        $('#weather_clouds').text(response.clouds.all);
+        $('#weather_wind').text(response.wind.speed);
+   
+      },
+    });
+  }
+})(jQuery);
